c6bc4f8ffbe787eefcc48e1fc995deb5f2495be7
toyman640 <falakoadebayor@gmail.com>
2024-04-30 15:02:17 +0100

    Fix linters errors

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 
-const Navigation = () => {
-
-  return (
-    <div className="Navigation">
-      <h2 className="Logo"> <span className="LogoSpace">LOGO</span></h2>
-      <div className="MenuContents">
-        <form action="" className="SearchForm">
-          <input className="SearchInput" type="text" placeholder="Search Property" />
-          <button className="SubmitButton">Search</button>
-        </form>
-        <ul className="MenuItems">
-          <li>Home</li>
-          <li>Rent</li>
-          <li>Sale</li>
-          <li>Contact</li>
-        </ul>
-      </div>
+const Navigation = () => (
+  <div className="Navigation">
+    <h2 className="Logo">
+      {' '}
+      <span className="LogoSpace">LOGO</span>
+    </h2>
+    <div className="MenuContents">
+      <form action="" className="SearchForm">
+        <input className="SearchInput" type="text" placeholder="Search Property" />
+        <button type="button" className="SubmitButton">Search</button>
+      </form>
+      <ul className="MenuItems">
+        <li>Home</li>
+        <li>Rent</li>
+        <li>Sale</li>
+        <li>Contact</li>
+      </ul>
     </div>
-  )
-}
+  </div>
+);
 
 export default Navigation;
