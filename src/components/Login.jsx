d0facfac55f82d6fcf35139156ff91a71b509040
toyman640 <falakoadebayor@gmail.com>
2024-05-01 11:45:39 +0100

    Add login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,16 @@ const Login = () => {
   return (
     <div>
       <h2>Login Page</h2>
+      <form action="">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+      </form>
     </div>
   );
 };
