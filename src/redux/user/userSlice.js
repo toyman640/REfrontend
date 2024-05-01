be172e9864c158afd3bbceda9b6d5525b2e85db1
toyman640 <falakoadebayor@gmail.com>
2024-05-01 13:13:52 +0100

    Add user slice to project

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -4,6 +4,49 @@ import axios from "axios";
 const LOGIN_URL = 'http://localhost:3000/login'
 
 const initialState = {
-  user: '',
+  user: null,
+  status: 'idle',
+  error: null,
 };
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(LOGIN_URL, { email, password });
+      return response.data;
+    } catch (error) {
+      // Handle login error
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => ({
+        ...state,
+        loading: true,
+        error: null,
+      }))
+      .addCase(loginUser.fulfilled, (state, action) => ({
+        ...state,
+        loading: false,
+        loginUser: action.payload,
+        error: null,
+        
+      }))
+      .addCase(loginUser.rejected, (state, action) => ({
+        ...state,
+        loading: false,
+        error: action.error.message,
+      }));
+  },
+});
+
+export default userSlice.reducer;
 
