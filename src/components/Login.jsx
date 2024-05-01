61a3cff61536c0f17e74a3d9f0c0e0a44631459d
toyman640 <falakoadebayor@gmail.com>
2024-05-01 11:57:51 +0100

    Add redux to project

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,6 +13,7 @@ const Login = () => {
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
         </div>
+        <input type="submit" value="Login" />
       </form>
     </div>
   );
