65d655f0bb8772ec828f877beb3bd46d545198e7
toyman640 <falakoadebayor@gmail.com>
2024-05-17 13:15:43 +0100

    Made menu responsive to scrolling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Navigation = () => (
-  <div className="Navigation">
-    <h2 className="Logo">
-      {' '}
-      <span className="LogoSpace">LOGO</span>
-    </h2>
-    <div className="MenuContents">
-      <form action="" className="SearchForm">
-        <input className="SearchInput" type="text" placeholder="Search Property" />
-        <button type="button" className="SubmitButton">Search</button>
-      </form>
-      <ul className="MenuItems">
-        <li>Home</li>
-        <li>Rent</li>
-        <li>Sale</li>
-        <li>Contact</li>
-      </ul>
-    </div>
-  </div>
-);
+const Navigation = () => {
+  const [isScrollingDown, setIsScrollingDown] = useState(false);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrollTop > lastScrollTop) {
+        // Scrolling down
+        setIsScrollingDown(true);
+      } else {
+        // Scrolling up
+        setIsScrollingDown(false);
+      }
+      setLastScrollTop(scrollTop <= 0 ? 0 : scrollTop); // For Mobile or negative scrolling
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollTop]);
+
+  return (
+    <>
+      <div className="Navigation">
+        <h2 className="Logo">
+          {' '}
+          <span className="LogoSpace">LOGO</span>
+        </h2>
+        <div className="MenuContents">
+          <form action="" className="SearchForm">
+            <input className="SearchInput" type="text" placeholder="Search Property" />
+            <button type="button" className="SubmitButton">Search</button>
+          </form>
+          <ul className="MenuItems">
+            <li>Home</li>
+            <li>Rent</li>
+            <li>Sale</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+      <div className={`MobileMenuBottom ${isScrollingDown ? 'hidden' : ''}`}>
+        <ul className="MobileMenuContents">
+          <li>Home</li>
+          <li>List</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
 
 export default Navigation;
