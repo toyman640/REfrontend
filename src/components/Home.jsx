f0d8701867f3e72228eb887f7c95f53763b7307e
toyman640 <falakoadebayor@gmail.com>
2024-05-20 15:05:38 +0100

    Create form for Desktop view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,8 +20,33 @@ const Home = () => {
             <h1 className="BannerMainText">MOST RELIABLE REAL ESTATE COMPANY.</h1>
             <p className="BannerBottomText">Many real estate firm can find you an apratment, let us find you a home without making you break a sweat.</p>
           </div>
-          <div>
+          <div className="FormSection">
+            <form action="" className="SearchForm">
+              <label htmlFor="" className="FormFieldLabel">
+                Deal Type <br />
+                <select name="" id="" className="FormField">
+                  <option value="">Rent</option>
+                  <option value="">Sale</option>
+                  <option value="">Lease</option>
+                  <option value="">Any</option>
+                </select>
+              </label>
+              <label htmlFor="" className="FormFieldLabel">
+                Property Category <br />
+                <select name="" id="" className="FormField">
+                  <option value="">Buildings</option>
+                  <option value="">Lands</option>
+                  <option value="">Residential</option>
+                  <option value="">Commercial</option>
+                </select>
+              </label>
+              <label htmlFor="" className="FormFieldLabel">
+                Maximun Price <br />
+                <input type="number" name="price" id="" className="FormInput" placeholder="300000"  />
+              </label>
 
+              <button type="button">Submit</button>
+            </form>
           </div>
         </div>
       </div>
