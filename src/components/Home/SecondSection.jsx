421d723fec9b4bf09afce9fd2a07b58d200fd11f
toyman640 <falakoadebayor@gmail.com>
2024-05-23 11:54:04 +0100

    Stlye card

--- src/components/Home/SecondSection.jsx
+++ src/components/Home/SecondSection.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getProperties } from '../../redux/property/propertySlice';
 import { Link } from 'react-router-dom';
+import { FaBath, FaBed, FaWallet  } from "react-icons/fa";
 
 const SecondSection = () => {
   const dispatch = useDispatch();
@@ -11,9 +12,13 @@ const SecondSection = () => {
   useEffect(() => {
     dispatch(getProperties());
   }, [dispatch]);
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat('en-US').format(price);
+  };
   return (
     <div className="SecondContainer">
-      <h3>Latest propertties</h3>
+      <h3 className="Ttile">LATEST PROPERTIES  <hr className="UnderLine" /></h3>
+     
       <div className="LatestPost">
         {newProperites.map((property) => (
           <div className="PropertyCard" key={property.id}>
@@ -29,9 +34,14 @@ const SecondSection = () => {
                   {property.ownership_type.name}
                 </p>
                 <p className="CardTiltle">{property.title}</p>
-                <p>{property.property_type.name}</p>
-                <p>{property.price}</p>
-                <p>{property.address}</p>
+                <p><FaWallet /> &#x20A6; {formatPrice(property.price)}</p>
+                <div className="MoreDeatils">
+                 <p><FaBath /> {property.no_of_bathrooms}</p>
+                 <p><FaBed /> {property.no_of_rooms}</p>
+                </div>
+                <p>{property.created_at}</p>
+                {/* <p>{property.address}</p> */}
+                
                 <p>
                   Posted  by :
                   {property.created_by.email}
